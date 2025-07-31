d65dbe79d749c6959a1985474b515bb3218b8ffd
How Bizarre <howbizarre@gmail.com>
2025-07-31 17:21:03 +0300

    feat: update button styles for improved visual consistency and add social media links in footer

--- app/components/button/Home.vue
+++ app/components/button/Home.vue
@@ -7,7 +7,7 @@ const localePath = useLocalePath();
   <NuxtLink :to="localePath('/')"
             :active-class="'nav-link-active'"
             role="button"
-            class="flex items-center justify-center rounded-xl p-2 text-sm font-medium transition-colors duration-150 ease-in hover:bg-gray-100 dark:hover:bg-gray-800">
+            class="flex items-center justify-center rounded-xl p-2 text-sm font-medium transition-colors duration-150 ease-in bg-gray-100 hover:bg-gray-300/75 dark:bg-gray-800/75 dark:hover:bg-gray-700">
     <UIcon name="i-heroicons-home" class="size-5" />
   </NuxtLink>
 </template>
--- app/components/button/Info.vue
+++ app/components/button/Info.vue
@@ -7,7 +7,7 @@ const localePath = useLocalePath();
   <NuxtLink :to="localePath('/info')"
             :active-class="'nav-link-active'"
             role="button"
-            class="flex items-center justify-center rounded-xl py-2 px-4 text-sm font-medium transition-colors duration-150 ease-in hover:bg-gray-100 dark:hover:bg-gray-800">
+            class="flex items-center justify-center rounded-xl py-2 px-4 text-sm font-medium transition-colors duration-150 ease-in bg-gray-100 hover:bg-gray-300/75 dark:bg-gray-800/75 dark:hover:bg-gray-700">
     {{ t('LBL_INFO') }}
   </NuxtLink>
 </template>
