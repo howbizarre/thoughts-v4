6e7deb868e809611dfd217369f678aa5ab584ba6
How Bizarre <howbizarre@gmail.com>
2025-08-01 13:38:37 +0300

    feat: add alwaysRedirect option for improved browser language detection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,7 +64,8 @@ export default defineNuxtConfig({
     detectBrowserLanguage: {
       useCookie: true,
       cookieKey: 'i18n_redirected',
-      redirectOn: 'root'
+      redirectOn: 'root',
+      alwaysRedirect: true
     },
     strategy: 'prefix',
     experimental: {
